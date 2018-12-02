b060fa060650cd2219880f024266150f684fbfc6
mbaker33 <mbaker33@gmail.com>
2018-12-01 20:47:48 -0500

    Adding New ToDos with functions

--- javascript/arrays/script.js
+++ javascript/arrays/script.js
@@ -1,19 +1,22 @@
+var todos = [];
 window.setTimeout(function() {
-    var todos = [];
-
     var input = prompt("What would you like to do?");
 
     while (input !== "quit"){
         //handle input
         if (input === "list") {
-            console.log(todos);
+            listToDos();
         }
         
         else if(input === "new") {
-            var newTodo = prompt("Enter new todo");
-            todos.push(newTodo);
-            console.log("todos");
+            newToDo();
         }
+
+        else if(input ==="delete") {
+            deleteToDo();
+            
+        }
+
         //ask again for new input
         var input = prompt("What would you like to do?");
     }
@@ -21,3 +24,24 @@ window.setTimeout(function() {
     console.log("OK, you quit the app...");    
 }, 500);
 
+function listToDos() {
+    console.log("Here is a list of your ToDos");
+    console.log("");
+    console.log("********");
+    todos.forEach(function listTodos(todo,index) {
+        console.log(index + " - " + todo);
+    })
+    console.log("********"); 
+}
+
+function newToDo() {
+    var newTodo = prompt("Enter new todo");
+    todos.push(newTodo);
+    console.log("ToDo Added");
+}
+
+function deleteToDo() {
+    var deleteTodo = prompt("Enter the index to delete.");
+    todos.splice(deleteTodo, 1);
+    console.log("ToDo deleted");
+}
